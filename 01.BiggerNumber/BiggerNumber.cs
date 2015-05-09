0bf8899c93719fc1b6efae699acbc0cb185e5073
DragomirBaynov <dbmotors.sofiq@gmail.com>
2015-05-09 17:40:49 +0300

    Update

--- 01.BiggerNumber/BiggerNumber.cs
+++ 01.BiggerNumber/BiggerNumber.cs
@@ -2,9 +2,30 @@
 
     class BiggerNumber
     {
+        static int GetMax(int a, int b)
+        {
+            int larger = int.MaxValue;
+
+            if (a>b)
+            {
+                larger = a;
+            }
+            else
+            {
+                larger = b;
+            }
+
+            return larger;
+
+        }
+
         static void Main()
         {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
 
+            int max = GetMax(firstNumber, secondNumber);
+            Console.WriteLine(max);
         }
     }
 
