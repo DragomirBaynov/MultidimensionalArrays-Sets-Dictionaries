f29ca78fb520b4e85d0def97a71a17460489235a
DragomirBaynov <dbmotors.sofiq@gmail.com>
2015-05-09 18:35:21 +0300

    03

---
+++ 03.LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class LargerThanNeighbours
+{
+    static bool IsLargerThanNeighbours(int[] input, int i)
+    {
+        bool result=true;
+        if (i > 0 && i < input.Length-1)
+        {
+            if (input[i] > input[i - 1] && input[i] > input[i + 1])
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+        }
+        else if(i==0)
+        {
+            if (input[i]>input[i+1])
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+        }
+        else if(i == input.Length)
+        {
+            if (input[i]>input[i-1])
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+        }
+        return result;
+    }
+    
+    static void Main()
+    {
+        int[] number = {1, 3, 4, 5, 1, 0, 5};
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.WriteLine(IsLargerThanNeighbours(number , i));
+        }
+    }
+}
+
